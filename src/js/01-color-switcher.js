45eb525c8c0793d80912722fb66703493a894815
Yana Ivanova <yaxo4ka1988@gmail.com>
2022-01-19 22:27:09 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,66 @@
-const startBtn = document.querySelector('.js-start');
-const stopBtn = document.querySelector('.js-stop');
-let timerId = null;
+// ======== СПОСОБ №2 ========
+const { startBtn, stopBtn, body, resetBtn } = refs;
 
-startBtn.addEventListener('click', () => {
-  timerId = setInterval(() => {
-    console.log(`I love async JS!  ${Math.random()}`);
-  }, 1000);
-});
+const INTERVAL_TIME = 900;
+let intervalId = null;
 
-stopBtn.addEventListener('click', () => {
-  clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
-});
+// - Вызовы слушателей (обработчиков) события
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
+resetBtn.addEventListener('click', resetBtnColorHandler);
+
+// - Генерации случайного цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// - Вешает рандомный цвет на body
+function getBodyColor() {
+  return (body.style.backgroundColor = getRandomHexColor());
+}
+
+// - Обработчик кнопки Старт
+function startBtnHandler({ target }) {
+  if (target.dataset.action.start) {
+    return;
+  }
+  console.log(target);
+
+  if (!target.disabled) {
+    target.disabled = true;
+
+    intervalId = setInterval(getBodyColor, INTERVAL_TIME);
+
+    resetBtn.classList.remove('is_active');
+    body.classList.remove('body_switcher');
+  }
+}
+
+// - Обработчик кнопки Стоп
+function stopBtnHandler({ target }) {
+  if (target.dataset.action.stop) {
+    return;
+  }
+  console.log(target);
+
+  clearInterval(intervalId);
+
+  if (startBtn.disabled) {
+    startBtn.disabled = false;
+    resetBtn.classList.add('is_active');
+  }
+}
+
+// - Сброс инлайн цвета
+function resetBtnColorHandler({ target }) {
+  if (target.dataset.action.reset) {
+    return;
+  }
+  console.log(target);
+
+  if (!body.classList.contains('body_switcher')) {
+    body.classList.add('body_switcher');
+    resetBtn.classList.remove('is_active');
+  }
+  return (body.style.backgroundColor = '');
+}
